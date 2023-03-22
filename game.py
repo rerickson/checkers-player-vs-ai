7a161d5c1a78090be690513ec09e7b64c8c50b44
Ryan Erickson <Ryan.Erickson4@gmail.com>
2023-03-22 14:31:31 -0700

    Fix for finding valid moves

--- game.py
+++ game.py
@@ -33,5 +33,5 @@ class Game:
             current_player = self.player2
 
         checker = self.board.get_checker(row, col)
-        if(checker != 0 and checker.player_number == self.turn):
-            current_player.click_checker(checker)
+        # if(checker != 0 and checker.player_number == self.turn):
+        current_player.click_checker(checker, row, col)
